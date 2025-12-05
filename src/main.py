3a0b463abe38e1fe976ffa82887326d4875ce4e9
Abdullah Al Muti <aamuti64@gmail.com>
2025-12-06 01:17:24 +0600

    add a custom middleware for logging

--- src/main.py
+++ src/main.py
@@ -1,12 +1,18 @@
+import logging
+import time
 from contextlib import asynccontextmanager
+from http.client import responses
 
 import uvicorn
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
+from starlette.requests import Request
 
 from src.core.config import settings
 from src.presentation.api import products
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -30,6 +36,15 @@ app.add_middleware(
     allow_methods=["*"],
     allow_header=["*"]
 )
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    logger.info(f"Path: {request.url.path} | Time: {process_time:.4f}s | Status: {response.status_code}")
+    return response
 
 app.include_router(products.router, prefix=settings.API_V1_STR)
 
