f0d047bb0799babe548f0ab0b605b6aa8bf7e767
Abdullah Al Muti <aamuti64@gmail.com>
2025-12-06 01:00:43 +0600

    add cors middleware

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from contextlib import asynccontextmanager
 
 import uvicorn
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from src.core.config import settings
 from src.presentation.api import products
@@ -16,6 +17,14 @@ async def lifespan(app: FastAPI):
 app = FastAPI(
     title=settings.PROJECT_NAME,
     lifespan=lifespan
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_header=["*"]
 )
 
 app.include_router(products.router, prefix=settings.API_V1_STR)
