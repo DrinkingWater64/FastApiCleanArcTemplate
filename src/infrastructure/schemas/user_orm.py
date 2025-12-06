5fbde4eee217f1788a7f25a233d516100a494a4d
Abdullah Al Muti <aamuti64@gmail.com>
2025-12-06 15:09:36 +0600

    added user orm for sqlAlchemy

---
+++ src/infrastructure/schemas/user_orm.py
@@ -0,0 +1,15 @@
+import uuid
+
+from sqlalchemy import UUID, String, Boolean
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.infrastructure.schemas.base_orm import Base
+
+
+class UserOrm(Base):
+    __tablename__ = "users"
+
+    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True)
+    email: Mapped[str] = mapped_column(String, unique=True, index=True, nullable=False)
+    password_hash: Mapped[str] = mapped_column(String, nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
